988d3e19db4177f02e2e6068f6cf577179c62ad5
ingluisgerardo <gerardo_2119@hotmail.com>
2016-06-24 13:36:31 -0500

    sdsad
    asdasd

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
-$host="localhost";
+$host="adminH5qP1BJ";
 $usuario="root";
-$password="";
+$password="1QA3aRLJ9zJA";
 $conectar=mysql_connect($host, $usuario, $password); 
 mysql_select_db("camiones",$conectar);
 
